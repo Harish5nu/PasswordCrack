77ae18a8fdced4e0fafbb590ec960b381a5290e6
Harish Saud <saudharish770@gmail.com>
2025-04-19 21:02:11 +0545

    Update passwordchecker.py

--- passwordchecker.py
+++ passwordchecker.py
@@ -19,19 +19,19 @@ def crack_hash(md5_hash, wordlist_path):
         return None
 
 def main():
-    print("Welcome to MD5 Hash Generator and Cracker!")
-    print("1. Generate MD5 Hash for a Password")
-    print("2. Crack An MD5 Hash using a Wordlist")
+    print("Welcome to MD5 Hash Generator and Cracker...!")
+    print("1. Generate MD5 Hash for a Password...")
+    print("2. Crack An MD5 Hash using a Wordlist..")
 
     option = input("Choose an option (1 or 2): ")
 
     if option == '1':
-        password = input("Enter the password to generate it's MD5 hash: ")
+        password = input("Enter the password to generate it's MD5 hash:- ")
         hashed_password = generate_md5(password)
         print(f"MD5 Hash of '{password}': {hashed_password}")
 
     elif option == '2':
-        md5_hash = input("Enter the MD5 hash for crack: ")
+        md5_hash = input("enter the MD5 hash for crack: ")
         wordlist_path = input("Enter the path to the wordlist file (e.g., 'rockyou.txt'): ")
         
         result = crack_hash(md5_hash, wordlist_path)
